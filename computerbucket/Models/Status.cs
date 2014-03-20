aadb03a9eb97a5e8df109bf0f9a4b57474c919ab
s00149173 <s00149173@mail.itsligo.ie>
2014-03-20 07:59:41 +0000

    Order Status, message for non existing email.

--- computerbucket/Models/Status.cs
+++ computerbucket/Models/Status.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,8 @@ namespace computerbucket.Models
     public class Status
     {
         public int OrderID { get; set; }
+        [Required(ErrorMessage = "Need value for email")]
+        [DataType(DataType.EmailAddress)]
         public string StatusField { get; set; }
         public string Name { get; set; }
     }
